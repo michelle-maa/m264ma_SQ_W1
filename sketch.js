2449c9c427fe42eab0e605c4595f57d637ef2a6a
michelle-maa <m264ma@uwaterloo.ca>
2026-05-18 22:47:44 -0400

    Added image, shape, and text.

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function preload() {
+  img = loadImage('/assets/images/Exit-8.jpg');
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  image(img, 0, 0, width, 300);
+  fill(255, 255, 0);
+  rect(145, 10, 100, 30); 
+  fill(0);
+  text("Exit 8", 180, 30);
+}
